7998e902b15a5e2fdb3252302fbaffa7b50ae725
GrimmettDS <davidsgrimmett@gmail.com>
2020-11-14 13:31:24 -0500

    adding a file

---
+++ HobbyBook.py
@@ -0,0 +1,15 @@
+# @TODO: Your code here
+
+People = {
+    "name": "David",
+    "nickname": "Dave",
+    "hobbies": ["baseball", "parenting", "coaching", "????"],
+    "days": {"Mon": 6, "Friday": 6, "Saturday": 6, "Sunday": 6}}
+
+
+print(f'Hello I am {People["name"]} and my nickname is {People["nickname"]}')
+print(f'I have {len(People["hobbies"])} hobbies! ')
+print(f'On the weekend I get up at  {People["days"]["Saturday"]}')
+
+
+
